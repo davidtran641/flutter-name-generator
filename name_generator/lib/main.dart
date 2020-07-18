cfb079b62266119d13cf28efa781ba4ceb512852
davidtran <davidtran641@gmail.com>
2020-07-18 23:40:05 +0800

    (FEAT) Add infinite ListView

--- name_generator/lib/main.dart
+++ name_generator/lib/main.dart
@@ -5,19 +5,8 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) =>
+      MaterialApp(title: 'Startup Name Generator', home: RandomWords());
 }
 
 class RandomWords extends StatefulWidget {
@@ -26,9 +15,32 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle textStyle = const TextStyle(fontSize: 18);
+
   @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: _buildSuggestions(context),
+      );
+
+  Widget _buildSuggestions(BuildContext context) => ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, index) => _buildListViewRow(context, index));
+
+  Widget _buildListViewRow(BuildContext context, int index) {
+    if (index.isOdd) {
+      return Divider();
+    }
+    final itemIndex = index ~/ 2;
+    if (itemIndex >= _suggestions.length) {
+      _suggestions.addAll(generateWordPairs().take(10));
+    }
+    return _buildRow(context, itemIndex);
   }
+
+  Widget _buildRow(BuildContext context, int itemIndex) => ListTile(
+      title: Text(_suggestions[itemIndex].asPascalCase, style: textStyle));
 }
